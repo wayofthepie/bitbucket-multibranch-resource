170d4de3846926e4cbf9b8dfd0722a19dc24576e
wayofthepie <wayofthepie@gmail.com>
2018-06-16 13:46:46 +0100

    Get basic in working

--- in.py
+++ in.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+import sys
+import json
 
-print("[{\"ref\":\"1234\"}]")
+outdir = sys.argv[1]
+source = json.loads(sys.stdin.read())
+version = source["version"]
+sys.stderr.write(str(version))
+
+f = open(outdir + "/versions", "w")
+f.write(json.dumps([version]))
+sys.stdout.write(json.dumps({"version": version, "metadata": []}))
